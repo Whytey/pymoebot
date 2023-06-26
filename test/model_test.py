60054a40de36fc35cafdd0f91a000a60fb434612
WhyTey <david@thewhytehouse.org>
2023-06-26 22:48:32 +1000

    Initial stab at some tests - mocking out tinytuya

--- test/model_test.py
+++ test/model_test.py
@@ -1,7 +1,27 @@
 import unittest
+from unittest import mock
+
 from pymoebot import MoeBotConnectionError, MoeBot
 
+
 class TestMoeBot(unittest.TestCase):
-    def test_no_device(self):
-        self.assertRaises(MoeBotConnectionError, MoeBot, "RANDOM", "127.0.0.1", "RANDOM_KEY_ABCDE")
-        
+    @mock.patch('tinytuya.Device.status')
+    def test_no_device(self, mock_status):
+        mock_status.return_value = {'Error': 'Network Error: Device Unreachable', 'Err': '905', 'Payload': None}
+
+        m = MoeBot("RANDOM", "127.0.0.1", "RANDOM_KEY_ABCDE")
+        self.assertFalse(m.online)
+        self.assertIsNone(m.state)
+
+    @mock.patch('tinytuya.Device.status')
+    def test_mock_device(self, mock_status):
+        mock_status.return_value = {
+            'dps': {'6': 100, '101': 'STANDBY', '102': 0, '103': 'MOWER_LEAN', '104': True, '105': 3, '106': 1111,
+                    '114': 'AutoMode'}}
+
+        m = MoeBot("RANDOM", "127.0.0.1", "RANDOM_KEY_ABCDE")
+        self.assertFalse(m.online)
+        self.assertIsNone(m.state)
+        m.poll()
+        self.assertTrue(m.online)
+        self.assertIsNotNone(m.state)
