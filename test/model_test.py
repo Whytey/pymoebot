aede9368f59757220d5c03bc52088e11b9af5ef0
WhyTey <david@thewhytehouse.org>
2023-06-26 21:53:44 +1000

    Add ability to manually poll the moebot and control the thread through the listen/unlisten methods.

--- test/model_test.py
+++ test/model_test.py
@@ -3,5 +3,5 @@ from pymoebot import MoeBotConnectionError, MoeBot
 
 class TestMoeBot(unittest.TestCase):
     def test_no_device(self):
-        self.assertRaises(MoeBotConnectionError, MoeBot("RANDOM", "127.0.0.1", "RANDOM_KEY_ABCDE"))
+        self.assertRaises(MoeBotConnectionError, MoeBot, "RANDOM", "127.0.0.1", "RANDOM_KEY_ABCDE")
         
