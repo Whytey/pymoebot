14327a1c61a78d2ad2972cc58c80ca3053a75286
WhyTey <david@thewhytehouse.org>
2023-07-16 20:50:39 +1000

    Fixup how we manage version numbers (and other metadata).

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 import setuptools
-from pymoebot import __version__
+import pymoebot.__about__ as about
 
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pymoebot",
-    version=__version__,
-    author="David Whyte",
-    author_email="David@Whyte.xyz",
-    description="A Python library intended to monitor and control MoeBot robotic lawn mowers.",
+    name=about.__title__,
+    version=about.__version__,
+    author=about.__author__,
+    author_email=about.__email__,
+    description=about.__summary__,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/Whytey/pymoebot',
+    url=about.__uri__,
     packages=setuptools.find_packages(),
     install_requires=[
         'tinytuya',  # Encryption - AES can also be provided via PyCrypto or pyaes
