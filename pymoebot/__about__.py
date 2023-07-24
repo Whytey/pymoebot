ef33288617d0d726c065bcbb7f06f97a1f69a760
WhyTey <david@thewhytehouse.org>
2023-07-24 21:54:58 +1000

    Fixup the reporting of updates to listeners.

--- pymoebot/__about__.py
+++ pymoebot/__about__.py
@@ -1,6 +1,6 @@
 __title__ = "pymoebot"
 __summary__ = "A Python library intended to monitor and control MoeBot robotic lawn mowers."
 __uri__ = "https://github.com/Whytey/pymoebot"
-__version__ = "%d.%d.%d" % (0, 2, 2)
+__version__ = "%d.%d.%d" % (0, 2, 3)
 __author__ = "David Whyte"
 __email__ = "David@Whyte.xyz"
