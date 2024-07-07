fdd86bc3776073ab24d66835a87360ffaa57c040
WhyTey <david@thewhytehouse.org>
2024-07-07 22:06:20 +1000

    Bump version for 0.3.1 release.

--- pymoebot/__about__.py
+++ pymoebot/__about__.py
@@ -1,6 +1,6 @@
 __title__ = "pymoebot"
 __summary__ = "A Python library intended to monitor and control MoeBot robotic lawn mowers."
 __uri__ = "https://github.com/Whytey/pymoebot"
-__version__ = "%d.%d.%d" % (0, 3, 0)
+__version__ = "%d.%d.%d" % (0, 3, 1)
 __author__ = "David Whyte"
 __email__ = "David@Whyte.xyz"
