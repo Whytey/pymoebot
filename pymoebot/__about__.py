94176c3f93c66a36e8e6e9dda3a35447f543b6c3
WhyTey <david@thewhytehouse.org>
2025-07-04 21:44:21 +1000

    Tweak version number

--- pymoebot/__about__.py
+++ pymoebot/__about__.py
@@ -1,6 +1,6 @@
 __title__ = "pymoebot"
 __summary__ = "A Python library intended to monitor and control MoeBot robotic lawn mowers."
 __uri__ = "https://github.com/Whytey/pymoebot"
-__version__ = "%d.%d.%d" % (0, 4, 0)
+__version__ = "%d.%d.%d" % (0, 3, 2)
 __author__ = "David Whyte"
 __email__ = "David@Whyte.xyz"
